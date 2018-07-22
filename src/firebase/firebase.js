113f32a2ac90c4e3cca5fc5744fa33308aef2a63
Prabhashankar Kannapan <shankar75031@gmail.com>
2018-07-22 14:03:56 +0530

    Added a few basic components

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const FirebaseConfig = {
+    apiKey: "AIzaSyDyNS50I-rg-7R_qtuzGoW_XxI7pLuNIRw",
+    authDomain: "react-todo-app-2b86b.firebaseapp.com",
+    databaseURL: "https://react-todo-app-2b86b.firebaseio.com",
+    projectId: "react-todo-app-2b86b",
+    storageBucket: "react-todo-app-2b86b.appspot.com",
+    messagingSenderId: "972612220868"
+};
+
+firebase.initializeApp(FirebaseConfig);
+const db = firebase.firestore();
+
+export default db;
